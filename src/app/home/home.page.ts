c4376c85fa2bec8958827af27801bf96931f5fde
ThiruppathiRamBabu <snthiru2000@gmail.com>
2023-08-09 16:43:11 +0530

    initial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import { LoadingController, Platform, ToastController } from '@ionic/angular';
+import { finalize } from 'rxjs';
+
+const IMAGE_DIR = 'stored-images';
+
+interface LocalFile {
+  name: string;
+  path: string;
+  data: string;
+}
+interface ApiResponse {
+  success: boolean;
+  message: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +23,155 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  images: LocalFile[] = [];
 
-  constructor() {}
+  constructor(private platform: Platform, private loadingCtrl: LoadingController, private http: HttpClient, private toastCtrl: ToastController) { }
 
+  async ngOnInit() {
+    this.loadFiles();
+  }
+
+  async loadFiles() {
+    this.images = [];
+
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading data...',
+    });
+    await loading.present();
+
+    Filesystem.readdir({
+      directory: Directory.Data,
+      path: IMAGE_DIR
+    }).then(result => {
+      console.log('here: ', result);
+      const fileNames = result.files.map(fileInfo => fileInfo.name);
+      this.loadFileData(fileNames);
+    }, async err => {
+      console.log('err: ', err);
+      await Filesystem.mkdir({
+        directory: Directory.Data,
+        path: IMAGE_DIR
+      });
+    }).then(_ => {
+      loading.dismiss();
+    })
+  }
+
+  async loadFileData(fileNames: string[]) {
+    for (let f of fileNames) {
+      const filePath = `${IMAGE_DIR}/${f}`;
+
+      const readFile = await Filesystem.readFile({
+        directory: Directory.Data,
+        path: filePath
+      });
+
+      this.images.push({
+        name: f,
+        path: filePath,
+        data: `data:image/jpeg;base64,${readFile.data}`
+      })
+      console.log('read', readFile);
+    }
+  }
+
+  async selectImage() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Prompt
+    });
+    console.log('img', image);
+
+    if (image) {
+      this.saveImage(image);
+    }
+  }
+
+  async saveImage(photo: Photo) {
+    const base64Data = await this.readAsBase64(photo);
+    console.log('base', base64Data);
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      directory: Directory.Data,
+      path: `${IMAGE_DIR}/${fileName}`,
+      data: base64Data
+    });
+    console.log('saved', savedFile);
+    this.loadFiles();
+  }
+
+  async readAsBase64(photo: Photo) {
+    if (this.platform.is('hybrid')) {
+      const file = await Filesystem.readFile({
+        path: photo.path!,
+      });
+      return file.data;
+    }
+    else {
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+      return await this.convertBlobToBase64(blob) as string;
+    }
+  }
+
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
+  async startUpload(file: LocalFile) {
+    const response = await fetch(file.data);
+    console.log('res', response);
+    const blob = await response.blob();
+    console.log('blob', blob);
+    const formData = new FormData();
+    formData.append('file', blob, file.name);
+    this.uploadData(formData);
+  }
+
+  async uploadData(formData: FormData) {
+    const loading = await this.loadingCtrl.create({
+      message: 'Uploading image...'
+    });
+    await loading.present();
+
+    const url = "http://localhost:81/images/upload.php";
+
+    this.http.post<ApiResponse>(url, formData).pipe(
+      finalize(() => {
+        loading.dismiss();
+      })
+    )
+      .subscribe(res => {
+        if (res['success']) {
+          this.presentToast('File upload complete.')
+        } else {
+          this.presentToast('File upload failed.')
+        }
+        console.log(res);
+      });
+  }
+
+  async deleteImage(file: LocalFile) {
+    await Filesystem.deleteFile({
+      directory: Directory.Data,
+      path: file.path
+    });
+    this.loadFiles();
+    this.presentToast('File removed.');
+  }
+  async presentToast(text: any) {
+    const toast = await this.toastCtrl.create({
+      message: text,
+      duration: 3000
+    });
+    toast.present();
+  }
 }
+
